8921f296227881d12dd75d9d01524fc6c6b2b1af
Mardoniosc <mardonio@live.com>
2020-11-10 14:42:19 -0300

    Adicionado logs em todas as mensagens

--- mensagens/mensagens.py
+++ mensagens/mensagens.py
@@ -1,14 +1,35 @@
 from urllib.request import urlopen
 from json import loads
+import re
+
 
 def get_frase_random():
-  req = "https://allugofrases.herokuapp.com/frases/random"
-  response = urlopen(req)
-  obj = loads(response.read())
-  mensagem = obj['frase'] + "\nLivro: " + obj['livro'] \
-              + "\nAuthor: " + obj['autor']
+    req = "https://allugofrases.herokuapp.com/frases/random"
+    response = urlopen(req)
+    obj = loads(response.read())
+    mensagem = obj["frase"] + "\nLivro: " + obj["livro"] + "\nAuthor: " + obj["autor"]
 
-  return mensagem
+    return mensagem
+
 
 def get_cumprimento_message(msg):
-  return 'Olá ' + msg.from_user.first_name + ', Legal poder falar com você'
+    return "Olá " + msg.from_user.first_name + ", Legal poder falar com você"
+
+
+def get_teste_message(msg):
+    return "Olá " + msg.from_user.first_name + "\nEstou funcionando bem"
+
+
+def get_contexto_frase(msg):
+    text = msg.text
+    if re.search("tudo bem", text, re.IGNORECASE) or re.search(
+        "tudo bom", text, re.IGNORECASE
+    ):
+        return "Eu sou um bot, e estou funcionando bem obrigado\n E você como está?"
+    elif re.search("estou bem", text, re.IGNORECASE):
+        return (
+            "Que bom!! em que posso lhe ajudar??"
+            "\n escolha a opção /start caso tenha duvida do que posso fazer"
+        )
+
+    return 'Não entendi o que quis dizer com: \n\n"' + text + '"'
--- menus/menuPrincipal.py
+++ menus/menuPrincipal.py
@@ -2,12 +2,14 @@
 import os
 from telebot import TeleBot
 
-from mensagens.mensagens import get_frase_random, get_cumprimento_message
+from mensagens.mensagens import *
+from logs.logs import atualizaLog
 
 bot = TeleBot(os.environ['BOT_TOKEN_FOSTER'])
 
 @bot.message_handler(commands=['start', 'help'])
 def send_start_message(message):
+  atualizaLog(message.text, message.from_user.first_name)
   bot.reply_to(message, 'Olá '+ message.from_user.first_name
                         +", eu sou o Foster "
                         "\nAbaixo a lista de comandos que eu respondo"
@@ -16,24 +18,41 @@ def send_start_message(message):
                         "\n\n/frase para mostrar uma frase"
                         "\n\n/start ou /help para mostrar os comandos novamente"
                         "\n\n /info para informações sobre bot e seu desenvolvedor"
+                        "\n\n /teste /test para teste de resposta"
                         )
 
 
 # opções escolhida do menu acima
 @bot.message_handler(commands=['frase'])
 def send_frase(message):
+  atualizaLog(message.text, message.from_user.first_name)
   frase = get_frase_random()
   bot.reply_to(message, frase)
 
 @bot.message_handler(commands=['info', 'donio'])
 def send_people(message):
+  atualizaLog(message.text, message.from_user.first_name)
   bot.reply_to(message, 'Olá meu nome é Foster\n'
                         'Fui criado por Mardonio S Costa com BotFather'
-                        '\nUltima atualização: 09/07/2020'
+                        '\nUltima atualização: 10/11/2020'
                         '\nCodigo fonte disponivel no github: https://github.com/Mardoniosc/botFoster'
                         '\nPara mais informações pode entrar em contato com @Mardoniosc no telegram'
                         )
 
 @bot.message_handler(commands=['oi', 'olá', 'oii', 'eae'])
 def send_cumprimento(message):
+    atualizaLog(message.text, message.from_user.first_name)
     bot.reply_to(message, get_cumprimento_message(message))
+
+
+@bot.message_handler(commands=['teste', 'test', 'tt'])
+def send_cumprimento(message):
+    atualizaLog(message.text, message.from_user.first_name)
+    bot.reply_to(message, get_teste_message(message))
+
+@bot.message_handler()
+def send_cumprimento(message):
+    atualizaLog(message.text, message.from_user.first_name)
+    bot.reply_to(message, get_contexto_frase(message))
+
+
